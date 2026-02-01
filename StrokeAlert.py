1e37df6330a5580ea85231e3984a33d1841c593b
Desmend Legra <dlegra11@wgu.edu>
2026-02-01 03:51:30 -0500

    Removed import sys. Added outer "inGame" loop. Replaced sys.exit with loop exits. Cleaned syntax. Added logit to check for valid stroke alert candidacy. Added option to call a stroke alert. Added confirmation for alert called/ not called. Added feedback/ explanation to stroke alert calls and patient candidacy. Added replay loop and option to replay or quit after completion.

--- StrokeAlert.py
+++ StrokeAlert.py
@@ -1,50 +1,99 @@
-import sys
 import random
-
-age = random.randint(45, 100)
-ageStr = (f"Age: {age}")
-
-symOnsetMinutes = random.randint(15, 720)
-symOnsetMinutesStr = (f"Last known well: {symOnsetMinutes} minutes")
-
-faceDroop = random.choice([True, False])
-if faceDroop == True:
-    faceDroopStr = "Facial Droop: Yes"
-else:
-    faceDroopStr = "Facial Droop: No"
-
-armDrift = random.choice([True, False])
-if armDrift == True:
-    armDriftStr = "Arm Drift: Yes"
-else:
-    armDriftStr = "Arm Drift: No"
-
-speechDiff = random.choice([True, False])
-if speechDiff == True:
-    speechDiffStr = "Speech Dif: Yes"
-else:
-    speechDiffStr = "Speech Dif: No"
-
-case = f"Information:\n {ageStr}\n {symOnsetMinutesStr}\n {faceDroopStr}\n {armDriftStr}\n {speechDiffStr}"
 
 input("Stroke Alert Simulator, Press enter to continue")
 
-start = input("Input 1 to start or 2 to quit: ")
-i = True
+inGame = True
+while inGame:
 
-while i:
-    if start.isdigit():
-        start_int = int(start)
-        if start_int == 1:
+    age = random.randint(22, 110)
+    ageStr = (f"Age: {age}")
+
+    symOnsetMinutes = random.randint(15, 360)
+    symOnsetMinutesStr = (f"Last known well: {symOnsetMinutes} minutes")
+
+    faceDroop = random.choice([True, False])
+    if faceDroop:
+        faceDroopStr = "Facial Droop: Yes"
+    else:
+        faceDroopStr = "Facial Droop: No"
+
+    armDrift = random.choice([True, False])
+    if armDrift:
+        armDriftStr = "Arm Drift: Yes"
+    else:
+        armDriftStr = "Arm Drift: No"
+
+    speechDiff = random.choice([True, False])
+    if speechDiff:
+        speechDiffStr = "Speech Dif: Yes"
+    else:
+        speechDiffStr = "Speech Dif: No"
+
+    case = f"Information:\n {ageStr}\n {symOnsetMinutesStr}\n {faceDroopStr}\n {armDriftStr}\n {speechDiffStr}"
+    caseParams = f"\n {ageStr}\n {symOnsetMinutesStr}\n {faceDroopStr}\n {armDriftStr}\n {speechDiffStr}"
+
+    while True:
+        start = input("Input 1 to start or 2 to quit: ")
+
+        if start == "1":
             print("Using the provided information, decide to call a stroke alert or not")
-            i = False
             break
-        elif start_int == 2:
-            sys.exit("Quitting")
-            i = False
+        elif start == "2":
+            print("Quitting simulator")
+            inGame = False
             break
         else:
             print("Invalid input")
-            start = input("Input 1 to start or 2 to quit: ")
+    if not inGame:
+        break
+    print(case)
+
+    isStrokeCandidate = (symOnsetMinutes <= 240 and (faceDroop or armDrift or speechDiff)  )
+
+    #Stroke alert should be called if the patient is within the window (4 hours) AND has at least one FAST symptom
+
+    callAlert = input("Would you like to call a stroke alert? (y/n)")
+    callAlert = callAlert.lower()
+
+    while True:
+        if callAlert == "y":
+            print("Stroke alert called!")
+            break
+        elif callAlert == "n":
+            print("No alert called")
+            break
+        else:
+            print("Invalid input")
+            callAlert = input("Would you like to call a stroke alert? (y/n)")
+            callAlert = callAlert.lower()
             continue
-print(case)
+
+    if callAlert == "y":
+        callAlertStr = "called a stroke alert"
+    else:
+        callAlertStr = "did not call a stroke alert"
+
+    print(f"You {callAlertStr} for this patient {caseParams}")
+
+    if isStrokeCandidate and callAlert == "y":
+        print("CORRECT! The patient was a stroke candidate and you correctly called the stroke alert!")
+    elif isStrokeCandidate and callAlert == "n":
+        print("The patient WAS a stroke candidate and the alert was missed!")
+    elif not isStrokeCandidate and callAlert == "y":
+        print("The patient was not a stroke candidate and the alert was incorrectly called!")
+    else:
+        print("CORRECT! The patient was not a stoke candidate")
+
+    if symOnsetMinutes <= 240 and (faceDroop or armDrift or speechDiff):
+        print("The patient was in the window with at least one FAST symptom")
+    elif symOnsetMinutes <= 240:
+        print("The patient was in the window but did not have any FAST symptoms")
+    else:
+        print("The patient was not in the window")
+
+    gameAgain = input("Would you like to play again? (y/n)").strip().lower()
+
+    if gameAgain != "y":
+        print("Exiting the simulator!")
+        break
+
